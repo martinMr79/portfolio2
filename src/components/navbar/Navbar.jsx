f0f8e34f8e4c5c04bec687be76a3e39533c2c38f
Martin Mroz <mamr@hotmail.no>
2023-06-09 00:05:41 +0200

    link for future use

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,8 +7,10 @@ function Navbar() {
     <Header>
       <Nav>
       <NavLink as={Link} to="/projects">Projects</NavLink>
+        <NavLink as={Link} to="/contact">Skills</NavLink>
         <NavLink as={Link} to="/about">About</NavLink>
         <NavLink as={Link} to="/contact">Contact</NavLink>
+        
       </Nav>
     </Header>
   );
