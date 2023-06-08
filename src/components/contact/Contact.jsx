8dddb722cedafd32ec64bb61080a389c66d26ab2
Martin Mroz <mamr@hotmail.no>
2023-06-09 01:52:40 +0200

    redoing contact form using yup, and mui

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,81 +1,66 @@
 import React from 'react';
-import { ContactSection, ContactLink, ContactList, FormContainer } from './Styled'; 
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { TextField, Button, Box } from '@mui/material';
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  message: yup.string().required(),
+});
+
+const ContactForm = () => {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = data => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box marginBottom={2}>
+        <Controller 
+          name="name"
+          control={control}
+          defaultValue=""
+          render={({ field }) => <TextField label="Name" fullWidth {...field} error={!!errors.name} helperText={errors.name?.message} />}
+        />
+      </Box>
+      <Box marginBottom={2}>
+        <Controller 
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field }) => <TextField label="Email" fullWidth {...field} error={!!errors.email} helperText={errors.email?.message} />}
+        />
+      </Box>
+      <Box marginBottom={2}>
+        <Controller 
+          name="message"
+          control={control}
+          defaultValue=""
+          render={({ field }) => <TextField label="Message" fullWidth multiline rows={4} {...field} error={!!errors.message} helperText={errors.message?.message} />}
+        />
+      </Box>
+      <Button variant="contained" type="submit">Submit</Button>
+    </form>
+  );
+};
+
+export default ContactForm;
 
 
+ 
+//            <ContactSection>
+//                <h2>Contact</h2>
+//
+//            <p>You can also reach me on:</p>
+//            <ContactList>
+ //               <li><ContactLink href="mailto:mamr@hotmal.no" title="e-mail" target="_blank" rel="noopener noreferrer"><i className="far fa-envelope"></i></ContactLink></li>
+//                <li><ContactLink href="tel:004790285550" title="phone" target="_blank" rel="noopener noreferrer"><i className="fas fa-phone"></i></ContactLink></li>
+//                <li><ContactLink href="linkedinProfileURL" title="linkedin" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></ContactLink></li>
+//                <li><ContactLink id="profile-link" href="https://github.com/martinMr79" title="github" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></ContactLink></li>
+//            </ContactList>
+//        </ContactSection>
 
-function Contact() {
-    const handleSubmit = e => {
-        e.preventDefault();
-        // Add your form handling code here
-    };
-
-        return (
-            <ContactSection id="contact">
-                <h2>Contact</h2>
-                <FormContainer>
-                <div>
-                    <div className="validform"></div>
-                </div>
-                <div className="container">
-                    <form id="contactForm" onSubmit={handleSubmit}>
-                    <div>
-                        <label>Enter your full name
-                            <input name="name" id="name" />
-                        </label>
-                        <div className="form-error" id="nameError">
-                            Please enter full name (min 6 characters)
-                        </div>
-                    </div>
-
-                    <div>
-                         <label
-                        >Subject
-                           <input name="subject" id="subject" />
-                       </label>
-                        <div class="form-error" id="subjectError">
-                        Please give me more details (min 16 characters long)
-                        </div>
-                    </div>
-
-                    <div>
-                        <label>
-                        Email is required
-                        <input name="email" id="email" />
-                        </label>
-                        <div class="form-error" id="emailError">
-                        Please enter a valid email address
-                        </div>
-                    </div>
-
-                    <div>
-            <label
-              >Message
-              <textarea
-                class="message-input"
-                name="message"
-                id="messageForm"
-              ></textarea>
-            </label>
-            <div class="form-error" id="messageError">
-              Message must be at least 26 characters long
-            </div>
-          </div>
-                    
-          <div className="button-container">
-                        <button className="contact-form-button" type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-            </FormContainer>
-            <p>You can also reach me on:</p>
-            <ContactList>
-                <li><ContactLink href="mailto:mamr@hotmal.no" title="e-mail" target="_blank" rel="noopener noreferrer"><i className="far fa-envelope"></i></ContactLink></li>
-                <li><ContactLink href="tel:004790285550" title="phone" target="_blank" rel="noopener noreferrer"><i className="fas fa-phone"></i></ContactLink></li>
-                <li><ContactLink href="linkedinProfileURL" title="linkedin" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></ContactLink></li>
-                <li><ContactLink id="profile-link" href="https://github.com/martinMr79" title="github" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></ContactLink></li>
-            </ContactList>
-        </ContactSection>
-    );
-}
-
-export default Contact;
