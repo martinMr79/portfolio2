218aab9e78cad0291de20a06b9ea52c8b5033ee2
Martin Mroz <mamr@hotmail.no>
2023-06-11 23:07:17 +0200

    adding fonts

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,6 +1,23 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
+import RobotoThin from "./assets/fonts/";
+//import RobotoThin from "./assets/fonts/Roboto-Thin.ttf";
+import MerriweatherSans from "./assets/fonts/MerriweatherSans-Regular.ttf";
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Roboto-Thin';
+    src: url(${RobotoThin}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'MerriweatherSans';
+    src: url(${MerriweatherSans}) format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  }
+
   h1 {
     font-size: 57px;
     font-family: 'Roboto-Thin', sans-serif;
