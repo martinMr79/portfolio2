c3d2d2224ba35361e3937882afc874f4919f22bf
Martin Mroz <mamr@hotmail.no>
2023-10-07 21:50:41 +0200

    added bitsAndBots project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import { ProjectSection, H2, ProjectsContainer, ProjectCard, ProjectImage, ProjectTitle, GithubLinksContainer, GithubLink, ProjectCardText} from './styled';
-import  HolidazeImage  from '../../assets/images/Holidaze.jpg';
+import HolidazeImage  from '../../assets/images/Holidaze.jpg';
 import EcomImage from '../../assets/images/Ecom.jpg';
 import BargainBasmentImage from '../../assets/images/BargainBasement.jpg';
+import bitsAndBotsImage from '../../assets/images/bitsAndBotsImage.PNG'
 
 function Projects() {
   return (
     <ProjectSection>
        <H2>Projects</H2>
       <ProjectsContainer>
+
+      <ProjectCard>  
+          
+          <ProjectTitle>Bits & Bots </ProjectTitle>
+        <ProjectImage src={bitsAndBotsImage} alt="Accommodation Booking Site" />
+        
+        <ProjectCardText> video game store website, built with react, zustand, and styled components, using Wordpress as API</ProjectCardText>
+    
+
+        <GithubLinksContainer>
+          <GithubLink
+            href="https://github.com/martinMr79/bits-and-bots"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Repo <i className="fab fa-github"></i>
+          </GithubLink>
+          <GithubLink
+            href="https://venerable-lebkuchen-cf4179.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Demo <i className="fa-solid fa-globe"></i>
+          </GithubLink>    
+      </GithubLinksContainer>
+    </ProjectCard>
         
         <ProjectCard>  
           
               <ProjectTitle>Accommodation Booking Site</ProjectTitle>
             <ProjectImage src={HolidazeImage} alt="Accommodation Booking Site" />
             
-            <ProjectCardText>accommodation booking site, built with react, zustand, MUI, using Noroff`s API.</ProjectCardText>
+            <ProjectCardText>accommodation booking site, built with react, zustand, and MUI, using Noroff`s API.</ProjectCardText>
         
 
             <GithubLinksContainer>
