34492497195e56c4c7bdd919474ae08dbd7d93ef
Martin Mroz <mamr@hotmail.no>
2023-12-05 14:09:57 +0100

    adjusting wheelThreshold

--- src/components/scrollHandler.js
+++ src/components/scrollHandler.js
@@ -4,23 +4,39 @@ import { useNavigate } from 'react-router-dom';
 const ScrollHandler = ({ routeOrder }) => {
   const navigate = useNavigate();
   const [wheelAccumulation, setWheelAccumulation] = useState(0);
-  const wheelThreshold = 50; // Adjust this threshold based on testing
-  const debounceTime = 500; // Time after which the accumulated value resets
+  const wheelThreshold = 200; // Threshold for triggering navigation
+  const debounceTime = 500; // Time to reset the accumulation
 
   const handleWheel = useCallback((event) => {
+    const isPageTallerThanViewport = document.body.offsetHeight > window.innerHeight;
+    const isAtTop = window.pageYOffset === 0;
+    const isAtBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+
+    // For shorter pages, always accumulate
+    if (!isPageTallerThanViewport) {
+      setWheelAccumulation(current => current + event.deltaY);
+      return;
+    }
+
+    // For taller pages, check position
+    if ((event.deltaY < 0 && isAtTop) || (event.deltaY > 0 && isAtBottom)) {
+      // Don't accumulate if at the extreme ends of the page
+      return;
+    }
+
     setWheelAccumulation(current => current + event.deltaY);
   }, []);
 
   const navigateOnThreshold = useCallback(() => {
+    const currentIndex = routeOrder.indexOf(window.location.pathname);
     if (Math.abs(wheelAccumulation) > wheelThreshold) {
-      const currentIndex = routeOrder.indexOf(window.location.pathname);
       if (wheelAccumulation > 0 && currentIndex < routeOrder.length - 1) {
         navigate(routeOrder[currentIndex + 1]);
       } else if (wheelAccumulation < 0 && currentIndex > 0) {
         navigate(routeOrder[currentIndex - 1]);
       }
     }
-    setWheelAccumulation(0); // Reset accumulation
+    setWheelAccumulation(0); 
   }, [wheelAccumulation, navigate, routeOrder]);
 
   useEffect(() => {
@@ -39,6 +55,8 @@ const ScrollHandler = ({ routeOrder }) => {
 };
 
 export default ScrollHandler;
+
+
 
 
 
