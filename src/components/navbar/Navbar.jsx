07b656d4d20ab38890eb01dfcdd93ddc84605811
Martin Mroz <mamr@hotmail.no>
2024-02-19 17:41:56 +0100

    link fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Header, Nav, NavLink, IconContainer } from './Styled'; 
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
 import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 
@@ -8,21 +9,38 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
+  // Define a custom NavLink component for react-scroll
+  const CustomScrollLink = ({ to, children }) => (
+    <ScrollLink
+      to={to}
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+      onClick={toggle}
+    >
+      {children}
+    </ScrollLink>
+  );
+
   return (
     <Header>
       <Nav isOpen={isOpen}>
         <IconContainer onClick={toggle}>
           {isOpen ? <CloseIcon fontSize="large" /> : <MenuIcon fontSize="large" />}
         </IconContainer>
-        <NavLink as={Link} to="/projects" isOpen={isOpen}>Projects</NavLink>
-        <NavLink as={Link} to="/about" isOpen={isOpen}>About</NavLink>
-        <NavLink as={Link} to="/contact" isOpen={isOpen}>Contact</NavLink>
+        <NavLink as="div">
+          <CustomScrollLink to="projectsSection">Projects</CustomScrollLink>
+        </NavLink>
+        <NavLink as={RouterLink} to="/about" onClick={toggle}>About</NavLink>
+        <NavLink as={RouterLink} to="/contact" onClick={toggle}>Contact</NavLink>
       </Nav>
     </Header>
   );
 }
 
 export default Navbar;
+
 
 
 
