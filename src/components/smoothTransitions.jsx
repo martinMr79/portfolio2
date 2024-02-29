32b4b27427a10d5c03e1183c1b0df6aa678d14c2
Martin Mroz <mamr@hotmail.no>
2024-02-29 20:27:56 +0100

    adjusted hover

--- src/components/smoothTransitions.jsx
+++ src/components/smoothTransitions.jsx
@@ -11,7 +11,8 @@ const FadeInSectionContainer = styled.div`
     `
     transition: all 1.2s ease; 
     &:hover {
-      box-shadow: 0 5px 15px rgba(0,0,0,0.2);
+      box-shadow: 0 10px 20px rgba(0,0,0,0.25); /* More pronounced shadow */
+  
     }
   `}
 `;
